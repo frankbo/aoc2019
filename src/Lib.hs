dd3e3b2369c783dca2479f3c638b17c5e6cfc1af
frankbo <frank.boeddeker@gmx.net>
2019-12-01 21:11:56 +0100

    Advent of Code day 1

---
+++ src/Lib.hs
@@ -0,0 +1,22 @@
+module Lib
+    ( dayOne
+    ) where
+
+dayOne :: IO ()
+dayOne = do words <- getWords "./src/input.txt"
+            print $ fuelForModules (map read words)
+
+getWords :: FilePath -> IO [String]
+getWords path = do contents <- readFile path
+                   return (lines contents)
+
+fuelForModules :: [ Integer ] -> Integer
+fuelForModules = foldl (\acc current -> acc + fuelForFuel (calculateFuel current)) 0
+
+calculateFuel :: Integer -> Integer
+calculateFuel fuel = (fuel `div` 3) - 2
+
+fuelForFuel :: Integer -> Integer
+fuelForFuel fuel
+   | calculateFuel fuel <= 0 = fuel
+   | otherwise = fuel + fuelForFuel (calculateFuel fuel)
