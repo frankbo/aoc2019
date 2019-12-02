74b19342ef8d7b4ce6c427345fbd7568198f2119
fboeddeker <frank.boeddeker@holidaycheck.com>
2019-12-02 14:29:27 +0100

    Reformat

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,22 +1,25 @@
 module Lib
-    ( dayOne
-    ) where
+  ( dayOne
+  ) where
 
 dayOne :: IO ()
-dayOne = do words <- getWords "./src/input.txt"
-            print $ fuelForModules (map read words)
+dayOne = do
+  words <- getWords "./src/input.txt"
+  print $ fuelForModules (map read words)
 
 getWords :: FilePath -> IO [String]
-getWords path = do contents <- readFile path
-                   return (lines contents)
+getWords path = do
+  contents <- readFile path
+  return (lines contents)
 
-fuelForModules :: [ Integer ] -> Integer
-fuelForModules = foldl (\acc current -> acc + fuelForFuel (calculateFuel current)) 0
+fuelForModules :: [Integer] -> Integer
+fuelForModules =
+  foldl (\acc current -> acc + fuelForFuel (calculateFuel current)) 0
 
 calculateFuel :: Integer -> Integer
 calculateFuel fuel = (fuel `div` 3) - 2
 
 fuelForFuel :: Integer -> Integer
 fuelForFuel fuel
-   | calculateFuel fuel <= 0 = fuel
-   | otherwise = fuel + fuelForFuel (calculateFuel fuel)
+  | calculateFuel fuel <= 0 = fuel
+  | otherwise = fuel + fuelForFuel (calculateFuel fuel)
